a8c0a463a3b139357d20d5fdefb70f249fb4d781
Hasham Asad <tipuasad2001@gmail.com>
2024-06-07 03:54:24 +0500

    build3

--- screens/MoviesListScreen.js
+++ screens/MoviesListScreen.js
@@ -5,9 +5,9 @@ import Row from "../components/Row";
 const renderItem = obj => <Row {...obj.item} />
 
 const MoviesListScreen = ({route}) => {
-
-    console.log(route.params)
+    
     const movies = route.params.Movies.Search;
+
     return (
         <FlatList
         renderItem={renderItem}
