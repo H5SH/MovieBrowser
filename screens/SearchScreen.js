f5f5f89c5f2e7bf1201e735951962e7e9694e42e
Hasham Asad <tipuasad2001@gmail.com>
2024-06-07 03:31:33 +0500

    done

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,37 +1,30 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { ImageBackground, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import SearchBox from '../components/SearchBox';
 import { useNavigation } from '@react-navigation/native';
 
-const image = {uri: 'https://legacy.reactjs.org/logo-og.png'};
 
-const SearchScreen = ()=> {
+const SearchScreen = () => {
 
   const navigation = useNavigation();
 
-  onSearch = async (searchText)=> {
-    try{
+  onSearch = async (searchText) => {
+    try {
       const response = await fetch(`https://omdbapi.com/?apikey=f96cdf02&s=${searchText}&page=10`)
       const json = await response.json()
       navigation.navigate('List', {
         Movies: json
       })
-    } catch(err) {
+    } catch (err) {
       console.log(err)
-    } 
+    }
   }
 
   return (
-
-    
-      <View style={styles.container}> 
-      <ImageBackground source={image} imageStyle={styles.image}>
-      <SearchBox onSearch={this.onSearch}/>
-      </ImageBackground>
-      </View>
-      
-    
+    <View style={styles.container}>
+      <SearchBox onSearch={this.onSearch} />
+    </View>
   )
 
 }
@@ -39,7 +32,7 @@ const SearchScreen = ()=> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: '',
+    alignItems: 'center',
     justifyContent: 'center',
   },
   image: {
